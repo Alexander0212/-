386353915491f95ed88739c6c1edf67f7ebabe2e
Alexander0212 <rotor02122004@gmail.com>
2023-05-07 10:35:29 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Бульбашкове_сортування
+{
+    partial class frmBubbleSort
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArray = new System.Windows.Forms.TextBox();
+            this.btGenerate = new System.Windows.Forms.Button();
+            this.dgvMass = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtBubbleSort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btBubbleSort = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMass)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtArray);
+            this.groupBox1.Controls.Add(this.btGenerate);
+            this.groupBox1.Controls.Add(this.dgvMass);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(1108, 350);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Генерація масиву";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введіть кількість елементів";
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(18, 99);
+            this.txtArray.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(148, 30);
+            this.txtArray.TabIndex = 3;
+            // 
+            // btGenerate
+            // 
+            this.btGenerate.Location = new System.Drawing.Point(439, 278);
+            this.btGenerate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btGenerate.Name = "btGenerate";
+            this.btGenerate.Size = new System.Drawing.Size(222, 36);
+            this.btGenerate.TabIndex = 6;
+            this.btGenerate.Text = "Згенерувати порожній масив";
+            this.btGenerate.UseVisualStyleBackColor = true;
+            this.btGenerate.Click += new System.EventHandler(this.btGenerate_Click);
+            // 
+            // dgvMass
+            // 
+            this.dgvMass.AllowUserToOrderColumns = true;
+            this.dgvMass.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMass.Location = new System.Drawing.Point(350, 33);
+            this.dgvMass.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvMass.Name = "dgvMass";
+            this.dgvMass.RowHeadersWidth = 51;
+            this.dgvMass.RowTemplate.Height = 24;
+            this.dgvMass.Size = new System.Drawing.Size(744, 234);
+            this.dgvMass.TabIndex = 5;
+            this.dgvMass.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMass_CellContentClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btBubbleSort);
+            this.groupBox2.Controls.Add(this.btExit);
+            this.groupBox2.Controls.Add(this.txtBubbleSort);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(18, 351);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(1124, 338);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "«Бульбашкова» сортування";
+            // 
+            // txtBubbleSort
+            // 
+            this.txtBubbleSort.Location = new System.Drawing.Point(375, 91);
+            this.txtBubbleSort.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBubbleSort.Name = "txtBubbleSort";
+            this.txtBubbleSort.Size = new System.Drawing.Size(552, 30);
+            this.txtBubbleSort.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(354, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Масив після сортування:";
+            // 
+            // btBubbleSort
+            // 
+            this.btBubbleSort.Location = new System.Drawing.Point(52, 82);
+            this.btBubbleSort.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btBubbleSort.Name = "btBubbleSort";
+            this.btBubbleSort.Size = new System.Drawing.Size(199, 144);
+            this.btBubbleSort.TabIndex = 7;
+            this.btBubbleSort.Text = "Сортувати";
+            this.btBubbleSort.UseVisualStyleBackColor = true;
+            this.btBubbleSort.Click += new System.EventHandler(this.btBubbleSort_Click);
+            // 
+            // btExit
+            // 
+            this.btExit.Location = new System.Drawing.Point(474, 185);
+            this.btExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(195, 66);
+            this.btExit.TabIndex = 8;
+            this.btExit.Text = "Вихід";
+            this.btExit.UseVisualStyleBackColor = true;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // frmBubbleSort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 653);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmBubbleSort";
+            this.Text = "Сортування «бульбашкою»";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMass)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtArray;
+        private System.Windows.Forms.TextBox txtBubbleSort;
+        private System.Windows.Forms.DataGridView dgvMass;
+        private System.Windows.Forms.Button btGenerate;
+        private System.Windows.Forms.Button btBubbleSort;
+        private System.Windows.Forms.Button btExit;
+    }
+}
+
